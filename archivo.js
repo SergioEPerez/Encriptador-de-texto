c7674b7e0e29619090d2fcb1938e39126962e67a
Exequiel perez <sergioe.perezlabougle@hotmail.com>
2022-05-10 18:43:03 -0300

    encriptador

--- archivo.js
+++ archivo.js
@@ -1,23 +1,23 @@
 function encriptar(){
-    var texto = document.querySelector(".textBox").value;
+    var texto = document.querySelector(".textodesencriptado").value;
     var textoCifrado = texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
-    document.querySelector(".rectangle1").innerHTML = textoCifrado;
-    document.querySelector(".textBox").value;
+    document.querySelector(".textoencriptado").innerHTML = textoCifrado;
+    document.querySelector(".textodesencriptado").value;
     }
 
 
     
 
 function desencriptar(){ 
-    var texto = document.querySelector(".textBox").value; 
+    var texto = document.querySelector(".textodesencriptado").value; 
     var textoCifrado = texto.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u"); 
-document.querySelector(".rectangle1").innerHTML= textoCifrado;
-document.querySelector(".textBox").value;
+document.querySelector(".textoencriptado").innerHTML= textoCifrado;
+document.querySelector(".textodesencriptado").value;
 }
 
 
 function copy(){
-    var content = document.querySelector(".rectangle1").innerHTML;
+    var content = document.querySelector(".textoencriptado").innerHTML;
 
     navigator.clipboard.writeText(content)
         .then(() => {
@@ -28,6 +28,7 @@ function copy(){
     })
  
 }
+
 
 
 
